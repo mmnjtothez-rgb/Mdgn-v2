56d3abc7518b2ea5e464437a857c34809c97fc08
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-17 22:01:24 +0330

    Delete src directory

--- src/screens/Home/HomeScreen.jsx
+++
@@ -1,8 +0,0 @@
-export default function HomeScreen() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the MDGN App.</p>
-    </div>
-  );
-}
--- src/utils/md5.js
+++
@@ -1,9 +0,0 @@
-// Lightweight MD5 implementation compatible with Vite builds
-export function md5(str) {
-  return crypto.subtle.digest("MD5", new TextEncoder().encode(str))
-    .then(buf => {
-      return Array.from(new Uint8Array(buf))
-        .map(x => x.toString(16).padStart(2, "0"))
-        .join("");
-    });
-}
